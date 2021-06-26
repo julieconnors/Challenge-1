10335ec822ce764479e0118b8b4aaf5ed6a13784
Juliana Connors <65951445+julieconnors@users.noreply.github.com>
2021-06-26 07:07:58 -0400

    Onramp React Training Challenge 1

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ScotchInfoBar from './ScotchInfoBar';
-import './styles.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import ScotchInfoBar from "./ScotchInfoBar";
+import "./styles.css";
 
 /**
  * Our data
  * ------------------------
  */
 const user = {
-  name: 'Chris on Code',
-  location: 'Las Vegas',
-  foodType: 'Everything',
+  name: "Chris on Code",
+  location: "Las Vegas",
+  foodType: "Everything",
   age: 28,
-  likes: 'Coding into the wee hours of the morning',
-  twitterUsername: 'chrisoncode',
+  likes: "Coding into the wee hours of the morning",
+  twitterUsername: "chrisoncode",
   avatar:
-    'https://scotch-res.cloudinary.com/image/upload/v1556479698/xRZsnhr0_400x400_cpyg2t.png'
+    "https://scotch-res.cloudinary.com/image/upload/v1556479698/xRZsnhr0_400x400_cpyg2t.png"
 };
 
 /**
@@ -25,13 +25,27 @@ const user = {
 function App() {
   return (
     <div className="App">
-      {/* Show user data here */}
-      <div className="user-deets">display user info here. happy coding!</div>
+      <div className="user-deets">
+        <img src={user.avatar} alt="user-avatar" />
+        <h3>
+          <a>{user.name}</a>
+        </h3>
+        <strong>Location</strong>
+        <p>{user.location}</p>
+        <strong>Eats</strong>
+        <p>{user.foodType}</p>
+        <strong>Age</strong>
+        <p>{user.age}</p>
+        <strong>Likes</strong>
+        <p>{user.likes}</p>
+        <strong>Twitter</strong>
+        <a href="">@{user.twitterUsername}</a>
+      </div>
 
       <ScotchInfoBar />
     </div>
   );
 }
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
